fcbea62e1a593fc63aec17a0a665adffcc767b22
gaizi <1424598066@qq.com>
2024-01-18 08:15:42 +0800

    done

--- Attendance.py
+++ Attendance.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python
 import rospy
+from sound_play.libsoundplay import SoundClient
 from opencv_apps.msg import FaceArrayStamped
 
 class Attendance:
     def __init__(self):
         rospy.init_node('attendance')
         rospy.on_shutdown(self.cleanup)
+
+        self.soundhandle = SoundClient()
+        rospy.sleep(1)
+
+        self.soundhandle.stopAll()
+		# rospy.loginfo("Ready, waiting for commands...")
         
         # Subscribe to the face detection output and set the callback function
         rospy.Subscriber('/face_recognition/output', FaceArrayStamped, self.face_recognition_callback)
@@ -14,12 +21,13 @@ class Attendance:
         self.unique_labels = set()
     
     def cleanup(self):
+        self.soundhandle.stopAll()
         rospy.loginfo("Shutting down node...")
 
     def face_recognition_callback(self, data):
         if data.faces:
             label = data.faces[0].label
-
+            # self.soundhandle.say("Good morning. How can I help you?")
             if label not in self.unique_labels:
                 file_path = '/home/mustar/Documents/attendance.txt'
 
@@ -28,6 +36,7 @@ class Attendance:
 
                 self.unique_labels.add(label)
                 rospy.loginfo("Label '{}' added to text file.".format(label))
+                self.soundhandle.say("Attendance recorded")
 
 if __name__=="__main__":
     try:
